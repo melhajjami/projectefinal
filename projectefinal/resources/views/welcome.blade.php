7237fab309c10614cad5f498dc68c2c7fba6ec07
amandeeparora99 <aarora8@boscdelacoma.cat>
2019-04-25 17:43:25 +0200

    Canviat

--- projectefinal/resources/views/welcome.blade.php
+++ projectefinal/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>nigger</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                <a href="https://laravel.com/docs">asdasd</a>
+                    <!-- Hola -->
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
