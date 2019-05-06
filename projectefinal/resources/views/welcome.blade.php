f2ad40779b948d1c73401139c616b20fac4c66fb
amandeeparora99 <aarora8@boscdelacoma.cat>
2019-05-06 15:28:29 +0200

    hey

--- projectefinal/resources/views/welcome.blade.php
+++ projectefinal/resources/views/welcome.blade.php
@@ -27,12 +27,13 @@
 
     <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-            <a class="navbar-brand mr-1" href="index.html">Start Bootstrap</a>
-        
-            <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-              <i class="fas fa-bars"></i>
-            </button>
-        </nav>
+      <a class="navbar-brand mr-1" href="index.html">Start Bootstrap</a>
+  
+      <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+        <i class="fas fa-bars"></i>
+      </button>
+      
+    </nav>
 
     <!-- /Navbar top -->
 
