ae1231540443cf903c78a48069211d37485d6cc2
amandeeparora99 <aarora8@boscdelacoma.cat>
2019-05-08 20:09:26 +0200

    hey

--- projectefinal/resources/views/prova.blade.php
+++ projectefinal/resources/views/prova.blade.php
@@ -1,58 +1,58 @@
-@extends("layouts.plantilla")
 
-<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
 <div class="container">
-    <style>
-    
+<div class="row">
+<h1>Twitter Profile Card</h1>
+<p>You can use with Twitter API, bot or manual.</p>
+<!-- code start -->
+<div class="twPc-div">
+    <a class="twPc-bg twPc-block"></a>
 
-/* ==========================================================================
-   Author's custom styles
-   ========================================================================== */
+	<div>
+		<div class="twPc-button">
+            <!-- Twitter Button | you can get from: https://about.twitter.com/tr/resources/buttons#follow -->
+            <a href="https://twitter.com/mertskaplan" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false" data-dnt="true">Follow @mertskaplan</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+            <!-- Twitter Button -->   
+		</div>
 
-body
-{
-    font-family: 'Open Sans', sans-serif;
-}
+		<a title="Mert S. Kaplan" href="https://twitter.com/mertskaplan" class="twPc-avatarLink">
+			<img alt="Mert S. Kaplan" src="https://mertskaplan.com/wp-content/plugins/msk-twprofilecard/img/mertskaplan.jpg" class="twPc-avatarImg">
+		</a>
 
-.fb-profile img.fb-image-lg{
-    z-index: 0;
-    width: 100%;  
-    margin-bottom: 10px;
-}
+		<div class="twPc-divUser">
+			<div class="twPc-divName">
+				<a href="https://twitter.com/mertskaplan">Mert S. Kaplan</a>
+			</div>
+			<span>
+				<a href="https://twitter.com/mertskaplan">@<span>mertskaplan</span></a>
+			</span>
+		</div>
 
-.fb-image-profile
-{
-    margin: -90px 10px 0px 50px;
-    z-index: 9;
-    width: 20%; 
-}
-
-@media (max-width:768px)
-{
-    
-.fb-profile-text>h1{
-    font-weight: 700;
-    font-size:16px;
-}
-
-.fb-image-profile
-{
-    margin: -45px 10px 0px 25px;
-    z-index: 9;
-    width: 20%; 
-}
-}</style>
-
-    @section("contingut")
-    
-    <div class="fb-profile">
-        <img class="fb-image-lg" src="http://lorempixel.com/850/280/nightlife/5/" alt="Profile image example"/>
-        <img class="fb-image-profile thumbnail" src="http://lorempixel.com/180/180/people/9/" alt="Profile image example"/>
-        <div class="fb-profile-text">
-            <h1>Eli Macy</h1>
-            <p>Girls just wanna go fun.</p>
-        </div>
-    </div>
-
-    @endsection
+		<div class="twPc-divStats">
+			<ul class="twPc-Arrange">
+				<li class="twPc-ArrangeSizeFit">
+					<a href="https://twitter.com/mertskaplan" title="9.840 Tweet">
+						<span class="twPc-StatLabel twPc-block">Tweets</span>
+						<span class="twPc-StatValue">9.840</span>
+					</a>
+				</li>
+				<li class="twPc-ArrangeSizeFit">
+					<a href="https://twitter.com/mertskaplan/following" title="885 Following">
+						<span class="twPc-StatLabel twPc-block">Following</span>
+						<span class="twPc-StatValue">885</span>
+					</a>
+				</li>
+				<li class="twPc-ArrangeSizeFit">
+					<a href="https://twitter.com/mertskaplan/followers" title="1.810 Followers">
+						<span class="twPc-StatLabel twPc-block">Followers</span>
+						<span class="twPc-StatValue">1.810</span>
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<!-- code end -->
+</div>
 </div>
