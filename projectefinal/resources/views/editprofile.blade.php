5b511e1875e4b1860246671c5a261bf467ac8e36
amandeeparora99 <aarora8@boscdelacoma.cat>
2019-05-10 18:39:57 +0200

    yeye

--- projectefinal/resources/views/editprofile.blade.php
+++ projectefinal/resources/views/editprofile.blade.php
@@ -6,15 +6,23 @@
     {{ csrf_field() }}
     {{ method_field('patch') }}
 
-    <input type="text" name="name"  value="{{ $user->name }}" />
+    <input type="text" name="name"  value="{{ $user->nom . ' ' . $user->cognom}}" />
 
     <input type="email" name="email"  value="{{ $user->email }}" />
 
-    <input type="password" name="password" />
+    <input type="text" name="nickname" value="{{ $user->nickname }}"/>
 
-    <input type="password" name="password_confirmation" />
+    <input type="text" name="fotoperfil" value="{{ $user->fotoperfil }}"/>
+
+    <input type="text" name="bg" value="{{ $user->background }}"/>
+
+    <input type="text" name="saldo" value="{{ $user->saldo }}"/>
+
+    <!-- <input type="password" name="password" />
+
+    <input type="password" name="password_confirmation" /> -->
 
     <button type="submit">Send</button>
 </form>
 
-@endsection
+@endesection
